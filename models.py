fixed gradient backflow issue, solve mnist with only a few hundered params

models.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch.multiprocessing import Pool
 from modules import ResistiveTensor, Reverb
@@ -20,13 +21,13 @@ class ReverbNetwork(torch.nn.Module):
             v_data = self.architecture.nodes[v]
             fxn = Reverb(u_data['shape'][2], u_data['shape'][3],
                          kernel_size=4, in_channels=u_data['shape'][1],
-                         out_channels=v_data['shape'][1])
+                         out_channels=v_data['shape'][1], init_plasticity=0.1)
             self.architecture.edges[(u, v)]['operator'] = fxn
         self.input_node = input_node
-        self.architecture.add_node(-1, state=torch.zeros(node_shape))
+        self.architecture.add_node(-1, state=torch.zeros(node_shape), shape=node_shape)
         self.architecture.add_edge(-1, self.input_node, operator=Reverb(node_shape[2], node_shape[3],
                                                                         kernel_size=4, in_channels=node_shape[1],
-                                                                        out_channels=node_shape[1]))
+                                                                        out_channels=node_shape[1], init_plasticity=0.1))
         self.inject_noise = inject_noise
 
     def parameters(self, recurse: bool = True):
@@ -61,6 +62,14 @@ class ReverbNetwork(torch.nn.Module):
             if n == -1:
                 continue
             self.architecture.nodes[n]['state'] = data['_future_state'].clone()
+
+    def detach(self):
+        for u, v, data in self.architecture.edges(data=True):
+            self.architecture.edges[(u, v)]['operator'].weight = torch.ones_like(data['operator'].weight) * .5
+            self.architecture.edges[(u, v)]['operator'].activation_memory = None
+        for n, data in self.architecture.nodes(data=True):
+            self.architecture.nodes[n]['state'] = torch.zeros(data['shape'])
+            self.architecture.nodes[n]['_future_state'] = torch.zeros(data['shape'])
 
 
 if __name__=='__main__':

modules.py
@@ -16,8 +16,7 @@ class Reverb(torch.nn.Module):
         super().__init__()
         if spatial1 != spatial2:
             raise ValueError("Only square spatial inputs expected currently.")
-        weight = torch.empty((1, in_channels, spatial1, spatial2))
-        self.weight = torch.nn.init.xavier_normal_(weight)
+        self.weight = torch.ones((1, in_channels, spatial1, spatial2)) * .5
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.spatial1 = spatial1

test.py
@@ -8,9 +8,9 @@ from matplotlib import pyplot as plt
 batch_size_train = 1
 batch_size_test = 1
 structure = nx.complete_graph(2, create_using=nx.DiGraph)
-for node in structure.nodes():
-    structure.add_edge(node, node)
-revnet = models.ReverbNetwork(structure, input_node=0, node_shape=(1, 3, 28, 28))
+# for node in structure.nodes():
+#     structure.add_edge(node, node)
+revnet = models.ReverbNetwork(structure, input_node=0, node_shape=(1, 2, 28, 28))
 train_loader = torch.utils.data.DataLoader(
     MNIST('./tmp/files/', train=True, download=True,
                                transform=torchvision.transforms.Compose([
@@ -29,20 +29,22 @@ test_loader = torch.utils.data.DataLoader(
                                ])),
     batch_size=batch_size_test, shuffle=True)
 
-revnet_decoder = torch.nn.Conv2d(kernel_size=28, in_channels=1, out_channels=10)
-present_frames = 3
-optimizer = torch.optim.Adam(lr=.01, params=list(revnet.parameters()) + list(revnet_decoder.parameters()))
+revnet_decoder = torch.nn.Sequential(torch.nn.MaxPool2d(2),
+                                     torch.nn.Conv2d(kernel_size=7, in_channels=1, out_channels=10))
+present_frames = 10
+optimizer = torch.optim.Adam(lr=.01, params=list(revnet_decoder.parameters()) + list(revnet.parameters()))
 ce_loss = torch.nn.NLLLoss()
 history = []
 for epoch in range(10):
     for i, (stim, target) in enumerate(train_loader):
         optimizer.zero_grad()
+        revnet.detach()
         if i > 100:
             break
         for i in range(present_frames):
             revnet.architecture.nodes[-1]['state'][:, 0, :, :] = stim
             revnet()
-        decode_input = revnet.architecture.nodes[1]['state'][:, 0, :, :][:, None, :, :].clone()
+        decode_input = revnet.architecture.nodes[0]['state'][:, 0, :, :][:, None, :, :].clone()
         y_hat = revnet_decoder(decode_input)
         y_hat = y_hat.view(1, 10)
         y_hat = torch.log_softmax(y_hat, dim=1)