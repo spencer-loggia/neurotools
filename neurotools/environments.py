fix gradient propogation issue, adjust default params, add dcgn tests

neurotools/environments.py
@@ -4,10 +4,14 @@ import networkx as nx
 from neurotools import models, geometry, modules, util
 
 
+def _set_mp_env():
+    torch.multiprocessing.set_start_method('spawn')
+
+
 class FuzzyMental:
 
     def __init__(self, target_beta_matrix, feature_names, atlas, roi_names, feature_generator, spatial, input_roi,
-                 stim_frames, generations=20, population=5, max_iter=200):
+                 stim_frames, generations=20, population=5, max_iter=1000):
         """
         Designed to fit a Reverb Network to brain data.
         :param target_beta_matrix: (voxels x features) from processed mri data
@@ -26,7 +30,8 @@ class FuzzyMental:
         self.atlas = atlas
         self.roi_names = roi_names
         self.stim_gen = feature_generator
-        self.corr, self.idx_roi_map, self.rdms = geometry.pairwise_rsa(target_beta_matrix, atlas, min_roi_dim=5)
+        self.corr, self.idx_roi_map, self.rdms = geometry.pairwise_rsa(target_beta_matrix, atlas,
+                                                                       min_roi_dim=5, ignore_atlas_base=False)
         self.ror_idx_map = {self.roi_names[idx]: i for i, idx in enumerate(self.idx_roi_map)}
         self.input_node = self.ror_idx_map[self.roi_names[input_roi]]
         self.stim_frames = stim_frames
@@ -43,65 +48,77 @@ class FuzzyMental:
                                                  input_node=self.input_node,
                                                  track_activation_history=True)
 
-    def beta_correlation_loss(self, run_list, verbose=True):
+    def beta_correlation_loss(self, model, run_list, verbose=True):
         """
         computes how well the network model matches functional representations in the brain
+        :param model: the reverb model
         :param run_list: order in which conditions were presented to network (num_frames x 1)
         :return: loss scalar, the total dissimilarity between representations at each node in `self.brain` with
                  representations at corresponding nodes in `self.structure`.
         """
         # this design matrix holds for all parallel stimuli in batch this epoch
-        design_matrix = torch.nn.functional.one_hot(run_list, num_classes=len(self.feature_names))
+        design_matrix = torch.nn.functional.one_hot(run_list, num_classes=len(self.feature_names)).float()
         loss = torch.Tensor([0.])
 
         # compute beta matrix from network activity, compute rdms on matrix, compare to brain rdms
-        for node, data in self.reverb_model.architecture.nodes(data=True):
+        for node, data in model.architecture.nodes(data=True):
             if node == -1:
                 continue
             time_course = torch.stack(data["_past_states"], dim=1).view(1, len(run_list), -1)  # batch x t x n
             betas = torch.transpose(torch.inverse(design_matrix.T @ design_matrix) @ design_matrix.T @ time_course,
                                     1,
                                     2)  # batch x n x k
-            betas = torch.mean(betas, dim=0)  # average out batch dimension
-            rdm = geometry.dissimilarity(betas, metric='dot')
-            target_brain_rdm = torch.Tensor(self.rdms[node])
+            betas = torch.mean(betas, dim=0).unsqueeze(0)  # average out batch dimension
+            rdm = geometry.dissimilarity(betas, metric='dot').squeeze()
+            target_brain_rdm = torch.Tensor(self.rdms[node]).squeeze()
             # compute the linear correlation between the model rdm at this node and the measured rdm at this roi
             local_loss = util.pearson_correlation(rdm, target_brain_rdm)
             loss = loss + local_loss
         # scale so loss stay comparable across prunings
-        loss = loss / len(self.reverb_model.nodes)
+        loss = loss / len(model.architecture.nodes)
         if verbose:
-            print("computed beta coef ficients")
+            print("computed beta coefficients")
         return loss
 
-    def _fit(self, reverb_model, lr=0.01):
-        optimizer = torch.optim.Adam(lr=lr, params=self.reverb_model.parameters())
+    def _fit(self, reverb_model, lr=0.001, verbose=True):
+        optimizer = torch.optim.Adam(lr=lr, params=reverb_model.parameters())
         epoch_history = []
         epoch = 0
-        while not util.is_converged(epoch_history, abs_tol=.01, consider=20) and epoch < self.max_iter:
+        while not util.is_converged(epoch_history, abs_tol=.001, consider=100) and epoch < self.max_iter:
             epoch += 1
-            batch, runlist = self.stim_gen.get_batch()
-            runlist = torch.Tensor(runlist)
+            batch, cond_list = self.stim_gen.get_batch()
             reverb_model.detach()
             optimizer.zero_grad()
-            for stim in batch:
+            runlist = []
+            for idx, stim in enumerate(batch):
                 for i in range(self.stim_frames):
-                    reverb_model.architecture.nodes[-1]['state'] = stim
+                    runlist.append(cond_list[idx])
+                    reverb_model.architecture.nodes[-1]['state'] = stim.unsqueeze(0)
                     reverb_model.forward()
-            loss = self.beta_correlation_loss(runlist)
+            loss = self.beta_correlation_loss(reverb_model, torch.Tensor(runlist).long())
             epoch_history.append(loss.detach().cpu().item())
-            loss.backward()
+            if verbose:
+                print("SL: Loss epoch", epoch, "is", epoch_history[-1])
+            # we want to maximize correlation so we negate loss
+            (-1 * loss).backward()
             optimizer.step()
         return epoch_history[-1]
 
-    def fit(self):
+    def fit(self, mp=True):
+        _set_mp_env()
         for generation in range(self.generations):
             print("**** SS: Generation", generation, "of", self.generations, "****")
             pop = [self.reverb_model.clone().mutate() for _ in range(self.population)]
-            with torch.multiprocessing.Pool() as p:
-                res = p.starmap(self._fit, pop)
+            if mp:
+                with torch.multiprocessing.Pool() as p:
+                    res = p.map(self._fit, pop)
+            else:
+                res = []
+                for m in pop:
+                    res.append(self._fit(m))
             res = torch.Tensor(res)
             print("SS: Generation fitness: ", res.detach().cpu().tolist())
-            best_idx = torch.argmin(res)
+            # choose model with highest correlation
+            best_idx = torch.argmax(torch.nan_to_num(res, nan=-1, posinf=-1, neginf=-1))
             self.reverb_model = pop[best_idx]
 

neurotools/geometry.py
@@ -81,4 +81,4 @@ def pairwise_rsa(beta: torch.Tensor, atlas: torch.Tensor, min_roi_dim=5, ignore_
     ind = torch.triu_indices(len(unique_filtered), len(unique_filtered), offset=1)
     adjacency[ind[0], ind[1]] = corr
     adjacency = adjacency.T + adjacency
-    return adjacency, unique_filtered, roi_dissimilarity, None
+    return adjacency, unique_filtered, roi_dissimilarity

neurotools/models.py
@@ -14,6 +14,7 @@ class ReverbNetwork(torch.nn.Module):
         self.architecture = structure.copy()
         self.activation = torch.nn.Sigmoid()
         self.tracking = track_activation_history
+        self.edge_module = edge_module
         self.device = None
         for node in self.architecture.nodes():
             state = torch.zeros(node_shape)
@@ -81,6 +82,7 @@ class ReverbNetwork(torch.nn.Module):
             self.architecture.nodes[n]['_future_state'] = torch.zeros(data['shape']).to(self.device)
             if self.tracking:
                 self.architecture.nodes[n]['_past_states'] = []
+        return self
 
     def to(self, device):
         self.device = device
@@ -90,6 +92,7 @@ class ReverbNetwork(torch.nn.Module):
         for n, data in self.architecture.nodes(data=True):
             self.architecture.nodes[n]['state'] = self.architecture.nodes[n]['state'].to(device)
             self.architecture.nodes[n]['_future_state'] = self.architecture.nodes[n]['_future_state'].to(device)
+        return self
 
     def mutate(self):
         weights = []
@@ -99,10 +102,11 @@ class ReverbNetwork(torch.nn.Module):
                 continue
             weight = data['operator'].get_weight().detach().cpu().item()
             weights.append(weight)
+            edges_ids.append((u, v))
         weights = torch.abs(torch.Tensor(weights))
         max_weight = torch.max(weights)
         weights = weights / max_weight
-        removal_probs = torch.pow(weights - 1, 2)
+        removal_probs = torch.pow(abs(weights - 1), 3) / 2
         for i, edge in enumerate(edges_ids):
             roll = torch.rand(size=(1,))
             if roll < removal_probs[i]:
@@ -110,7 +114,7 @@ class ReverbNetwork(torch.nn.Module):
                 print("removed edge (", edge[0], ",", edge[1], " with roll of", roll.item(),
                       "against chance of", removal_probs[i].item())
         # add edge modifier
-        add_prob = 1 / (len(self.architecture.nodes)**2)
+        add_prob = 1 / (len(self.architecture.nodes)**3)
         for u, u_data in self.architecture.nodes(data=True):
             for v, v_data in self.architecture.nodes(data=True):
                 roll = torch.rand(size=(1,))
@@ -121,8 +125,9 @@ class ReverbNetwork(torch.nn.Module):
                     self.architecture.add_edge(u, v, operator=fxn)
                     print("inserted edge (", u, ",", v, " with roll of", roll.item(),
                           "against chance of", add_prob)
+        return self
 
     def clone(self):
+        self.detach()
         new_net = copy.deepcopy(self)
-        new_net.detach()
         return new_net

neurotools/modules.py
@@ -112,44 +112,49 @@ class WeightedConvolution(torch.nn.Module):
     def __init__(self, spatial1, spatial2, kernel_size, in_channels, out_channels, device='cpu', **kwargs):
         super().__init__()
         self.kernel_size, self.pad = util.conv_identity_params(in_spatial=spatial1, desired_kernel=kernel_size)
-        conv = torch.empty((out_channels, in_channels, kernel_size, kernel_size))
+        conv = torch.empty((out_channels, in_channels, self.kernel_size, self.kernel_size))
         self.conv = torch.nn.Parameter(torch.nn.init.xavier_normal_(conv)).to(device)
-        self.out_edge = torch.nn.Parameter(torch.normal(size=(1,), mean=.5, std=.1)).to(device)
+        self.out_edge = torch.nn.Parameter(torch.normal(size=(1,), mean=0., std=.1)).to(device)
         self.tanh = torch.nn.Tanh()
         self.unfolder = torch.nn.Unfold(kernel_size=self.kernel_size, padding=self.pad)
         self.folder = torch.nn.Fold(kernel_size=self.kernel_size, padding=self.pad,
                                     output_size=(spatial1, spatial2))
+        self.out_channel = out_channels
+        self.spatial1 = spatial1
+        self.spatial2 = spatial2
+        self.device = device
 
     def forward(self, x):
+        if len(x.shape) != 4:
+            raise ValueError("Input Tensor Must Be 4D, not shape", x.shape)
         if torch.max(x) > 1 or torch.min(x) < 0:
             print("WARN: Reverb  input activations are expected to have range 0 to 1")
         # unfold x to synaptic space
-        xufld = self.unfolder(x)
-        conv_weight = torch.abs(self.conv.clone())
-        conv_res = (xufld.transpose(1, 2) @ conv_weight.view(conv_weight.size(0), -1).t()).transpose(1, 2)
-        conv_res = self.folder(conv_res)
+        xufld = self.unfolder(x).transpose(1, 2)
+        conv_weight = torch.abs(self.conv.clone()).view(self.conv.size(0), -1).t()
+        conv_res = (xufld @ conv_weight).transpose(1, 2)
+        conv_res = conv_res.view(-1, self.out_channel, self.spatial1, self.spatial2)
         # conv res is standardized so weight comes from edge
         conv_res = conv_res / torch.std(conv_res)
-        weighted_out = conv_res * self.out_edge
+        weighted_out = conv_res * self.out_edge.clone()
         return weighted_out
 
     def get_weight(self):
         return self.out_edge
 
-    def update(self):
+    def update(self, *args):
         """
         No intrinsic update for this edge module type.
         """
         pass
 
     def detach(self):
-        self.conv = self.conv.detach().clone()
-        self.out_edge = self.out_edge.detach().clone()
         return self
 
     def to(self, device):
         self.conv.to(device)
         self.out_edge.to(device)
+        self.device = device
         return self
 
 