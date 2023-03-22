e6bb7f774a8f114afb2575e141058382c0308b09
Spencer R. Loggia <spencer.loggia98@gmail.com>
2023-03-22 17:57:13 -0400

    batched generator euclidean selector

--- neurotools/generators.py
+++ neurotools/generators.py
@@ -1,5 +1,8 @@
+import math
 import random
+from typing import List
 
+import numpy as np
 import torch
 
 
@@ -8,38 +11,25 @@ class CategoricalDistributor:
     helper class that distributes a given number of classes over the surface of a unit hypersphere. used to convert
     continuous action into logits.
     """
-    def __init__(self, initial_classes, device='cpu'):
+
+    def __init__(self, class_labels: List[int], device='cpu'):
         self.num_classes = 0
-        self.loci = []
-        self.names = []
-        self.device = device
-        for name in initial_classes:
-            self.add_class(name=name)
-        self.optim = torch.optim.Adam(lr=.001, params=self.loci)
+        self.labels = torch.Tensor(class_labels)
+        self.loci = [torch.Tensor([math.cos(2 * math.pi / i), math.sin(2 * math.pi / i)])
+                     for i in range(self.labels.shape[0])]
 
-    def add_class(self, name=None):
-        self.num_classes += 1
-        locus = torch.nn.Parameter((torch.rand(size=(2,)) * 2) * torch.pi) # range: 0, 2*pi
-        self.loci.append(locus)
-        self.names.append(name)
-
-    def logits(self, polar):
+    def logits(self, selected):
         """
-        :param polar: batch x 2 tensor of theta, psi spherical coordinates
+        :param selected: batch x 2 tensor of x, y coordinates
         :return: batch x num_classes distance from each locus along surface.
         """
-        loci = torch.stack(self.loci).to(self.device)
-        polar = torch.remainder(polar, torch.pi * 2)
-        theta = polar[:, 0][:, None]
-        theta2 = loci[:, 0][None, :]
-        psi = polar[:, 1][:, None]
-        psi2 = loci[:, 1][None, :]
-        arc = torch.acos(torch.sin(theta)*torch.sin(theta2) + torch.cos(theta) * torch.cos(theta2) * torch.cos(psi - psi2))
-        arc[arc > torch.pi] = 2 * torch.pi - arc[arc > torch.pi]
-        return arc
+        loci = torch.stack(self.loci)
+        dist = torch.cdist(selected, loci, p=2.0)
+        dist = dist.reshape((selected.shape[0], loci.shape[0]))
+        return dist
 
 
-class BasicMultiClass:
+class BasicMultiClassRL:
     """
     five synthetic 4x4 classes for testing
     """
@@ -51,7 +41,6 @@ class BasicMultiClass:
         self.noise = noise
         self.label_min = label_min
         self.label_max = label_max
-        self.selector = CategoricalDistributor(initial_classes=self.class_names[label_min:label_max], device=dev)
         self.device = dev
         self.ce = torch.nn.CrossEntropyLoss()
         self.min_loss = 0
@@ -61,64 +50,53 @@ class BasicMultiClass:
         bad_hat[0] = 0
         self.max_loss = self.ce(bad_hat, gt_ex)
         self.chance_loss = self.ce(torch.ones((label_max - label_min)), gt_ex)
+        self.selector = CategoricalDistributor(list(range(label_min, label_max)))
+        self.class_templates = torch.Tensor([[[0, 1, 0, 0],
+                                              [1, 1, 1, 1],
+                                              [0, 1, 0, 0],
+                                              [0, 1, 0, 0]],
+                                             [[1, 0, 0, 0],
+                                              [0, 1, 0, 0],
+                                              [0, 0, 1, 0],
+                                              [0, 0, 0, 1]],
+                                             [[0, 1, 0, 1],
+                                              [1, 0, 1, 0],
+                                              [0, 1, 0, 1],
+                                              [1, 0, 1, 0]],
+                                             [[1, 1, 1, 1],
+                                              [0, 0, 0, 0],
+                                              [1, 1, 1, 1],
+                                              [0, 0, 0, 0]],
+                                             [[0, 1, 0, 0],
+                                              [0, 1, 0, 0],
+                                              [0, 1, 0, 0],
+                                              [0, 1, 0, 0]]])
         self.prev_target = None
 
     def __len__(self):
         return self.num_samples
 
-    def pop(self):
-        # Generate a random label
-        label = torch.randint(self.label_min, self.label_max, (1,))
+    def pop(self, batch_size: int = 1):
+        # Generate a random label batch
+        labels = torch.randint(self.label_min, self.label_max, (batch_size,))
+        images = self.class_templates[labels].detach().clone()
+        images = images + torch.normal(size=images.shape, mean=0, std=self.noise, device=self.device)
+        return images, labels.long().to(self.device)
 
-        if label == 0:
-            # Generate an up/down cross image
-            image = torch.tensor([[0, 1, 0, 0],
-                                  [1, 1, 1, 1],
-                                  [0, 1, 0, 0],
-                                  [0, 1, 0, 0]], device=self.device)
-        elif label == 1:
-            # Generate a diagonal line image
-            image = torch.tensor([[1, 0, 0, 0],
-                                  [0, 1, 0, 0],
-                                  [0, 0, 1, 0],
-                                  [0, 0, 0, 1]], device=self.device)
-        elif label == 2:
-            # Generate a checkerboard image
-            image = torch.tensor([[0, 1, 0, 1],
-                                  [1, 0, 1, 0],
-                                  [0, 1, 0, 1],
-                                  [1, 0, 1, 0]], device=self.device)
-        elif label == 3:
-            # Generate horizontal lines image
-            image = torch.tensor([[1, 1, 1, 1],
-                                  [0, 0, 0, 0],
-                                  [1, 1, 1, 1],
-                                  [0, 0, 0, 0]], device=self.device)
-        else:
-            # Generate a vertical line image
-            image = torch.tensor([[0, 1, 0, 0],
-                                  [0, 1, 0, 0],
-                                  [0, 1, 0, 0],
-                                  [0, 1, 0, 0]], device=self.device)
-
-        image = image + torch.normal(size=image.shape, mean=0, std=self.noise, device=self.device)
-
-        return image, label.long().to(self.device)
-
-    def poll(self, action=None):
+    def poll(self, batch=1, action=None):
         loss = None
-        state, label = self.pop()
+        state, label = self.pop(batch)
         if action is not None:
             logits = self.selector.logits(action)
-            loss = self.ce(logits, self.prev_target)
+            loss = self.ce(logits, label)
         self.prev_target = label
         return state, loss
 
-    def play(self, model):
+    def play(self, model, batch_size=1):
         jitter_samples = self.num_samples + random.randint(0, int(self.num_samples / 5))
         action = None
         for i in range(jitter_samples):
-            state, loss = self.poll(action)
+            state, loss = self.poll(action=action, batch=batch_size)
             if loss is not None:
                 loss = loss / jitter_samples
             action = model.delta(state)
