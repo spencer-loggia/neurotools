cb6c9d908584217cf0d18c569f8c42b0008a9b42
Spencer R. Loggia <spencer.loggia98@gmail.com>
2024-02-04 12:13:24 -0500

    add testcases to git

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -4,7 +4,7 @@ import torch
 
 def test_elegant_weighted_convolution():
     states = torch.sigmoid(torch.normal(mean=0, std=.5, size=(4, 2, 16, 16)))
-    mod = modules.ElegantWeightedConvolution(in_channels=2, out_channels=2, spatial1=16, spatial2=16,
+    mod = modules.ElegantWeightedConvolution(channels=2, spatial1=16, spatial2=16,
                                              kernel_size=4, num_nodes=4)
     out = mod(states)
     print('here')
@@ -12,7 +12,7 @@ def test_elegant_weighted_convolution():
 
 def test_einsum_solution_simple():
     states = torch.zeros(size=(2, 1, 1, 1)) + .5
-    mod = modules.ElegantWeightedConvolution(in_channels=1, out_channels=1, spatial1=4, spatial2=4,
+    mod = modules.ElegantWeightedConvolution(channels=1, spatial1=4, spatial2=4,
                                              kernel_size=3, num_nodes=2, normalize_conv=False)
     mod.spatial1 = 1
     mod.spatial2 = 1
