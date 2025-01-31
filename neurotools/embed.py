b17dc804512ae72b027803228a6f7fc8f0542951
Spencer R. Loggia <spencer.loggia98@gmail.com>
2025-01-31 16:04:55 -0500

    at representational geomerty functionality

---
+++ neurotools/embed.py
@@ -0,0 +1,217 @@
+import numpy as np
+import torch
+from neurotools import util
+
+
+class MDScale:
+
+    def __init__(self, n, embed_dims: int = 2, device='cpu'):
+        """
+        Computes an embedding of n examples into a `embed_dims` space that attempts to maintain the provided pairwise
+        distances between examples
+        :param n: number of items
+        :param embed_dims: number of dimensions to construct space in
+        :param device: device to use
+        """
+        self.num_items = n
+        self.components = embed_dims
+        self.mse = torch.nn.MSELoss()
+        self.right_latent = None
+        self.left_latent = None
+        self.device = device
+        self.stress_history = None
+
+    def to(self, device):
+        self.device = device
+        return self
+
+    def check_dists(self, dist):
+        """
+        function to check if distances are a vector or matrix, if a matrix returns just the flattened upper triangle if
+        symetric, or a tuple of upper and lower (flat) triangles if not symmetric.
+        :param num_items: number of items pairwise dists are for. (num rows and cols of distance matrix)
+        :param dist:
+        :return:
+        """
+        if isinstance(dist, np.ndarray):
+            dist = torch.from_numpy(dist).float()
+        if np.ndim(dist) == 2 and dist.shape[0] == self.num_items and dist.shape[1] == self.num_items:
+            # is a square distance matrix
+            sym = (dist.T == dist).all()
+            items = dist.shape[0]
+            upinds = torch.triu_indices(items, items, offset=1)
+            if sym:
+                dists = (dist[upinds[0], upinds[1]],)
+            else:
+                print("Similarity graph is directed. Embedding in and out dissimilarity separately.")
+                linds = torch.tril_indices(items, items, offset=-1)
+                dists = (dist[upinds[0], upinds[1]],
+                         dist[linds[0], linds[1]])
+
+        elif np.ndim(dist) == 1 and (dist.shape[0] * 2) / (self.num_items - 1) == self.num_items:
+            # is a triangular distance vector
+            dists = (dist,)
+        else:
+            raise ValueError("Provided distance matrix / vector is malformed.")
+        return dists
+
+    def stress(self, pairwise_target, positions, order=2):
+        """
+        An L2 Norm between distance in embedding space an actual provided pairwise distances
+        :param positions:
+        :param pairwise_target: upper triangular vector of pairwise distance between examples, size n(n-1) / 2
+        :return: torch.Tensor a stress score for the system
+        """
+        cur_dists = torch.pdist(positions)
+        # stress = self.mse(cur_dists, pairwise_target)
+        stress = torch.abs(cur_dists - pairwise_target)
+        stress = torch.pow(stress, order)
+        stress = torch.mean(stress)
+        return stress
+
+    def embed(self, dist_vec, max_iter=2000, tol=.001):
+        history = []
+        cur_iter = 0
+        embedding = torch.empty((self.num_items, self.components)).to(self.device)
+        embedding = torch.nn.Parameter(torch.nn.init.xavier_normal_(embedding))
+        optimizer = torch.optim.Adam(lr=.1, params=[embedding])
+        dist_vec = dist_vec.to(self.device)
+        cur_iter = 0
+        converged = False
+        # loss_history, optim, batch_size, t
+        while not converged:
+            optimizer, converged = util.is_converged(history, optimizer, 1, cur_iter, max_lr=1000)
+            if cur_iter >= max_iter:
+                print("WARNING: Failed to converge in", max_iter, "iterations. Could be a solution was found, but the "
+                                                                  "convergence tracker is dumb, just be careful!.")
+                break
+            optimizer.zero_grad()
+            loss = self.stress(dist_vec, embedding)
+            history.append(loss.detach().cpu().item())
+            loss.backward()
+            optimizer.step()
+            cur_iter += 1
+        self.stress_history = history
+        return embedding
+
+    def fit(self, pairwise_distance: torch.Tensor, max_iter=10000, tol=.001):
+        dists = self.check_dists(pairwise_distance)
+        self.right_latent = self.embed(dists[0], max_iter=max_iter)
+        print("Right Initial System Tension: ", self.stress_history[0])
+        print("Right Final System Tension: ", self.stress_history[-1])
+        if len(dists) == 2:
+            self.left_latent = self.embed(dists[1], max_iter=max_iter, tol=tol)
+            print("Left Initial System Tension: ", self.stress_history[0])
+            print("Left Final System Tension: ", self.stress_history[-1])
+
+    def fit_transform(self, pairwise_distance, max_iter=10000, tol=.001):
+        self.fit(pairwise_distance, max_iter=max_iter, tol=tol)
+        return self.predict()
+
+    def predict(self):
+        if self.left_latent is None:
+            return self.right_latent.detach().cpu()
+        else:
+            return self.right_latent.detach().cpu(), self.left_latent.detach().cpu()
+
+
+class SupervisedEmbed:
+
+    def __init__(self, n_components=2, device='cpu', dist_metric="euclidian",
+                 intra_class_weight=1., inter_class_weight=1., sparsity=1):
+        """
+        Finds three feature weight vectors that maximize the distance between the classes while minimizing the variance
+        within each class and maintaining component sparsity.
+        Used in "Loggia, S. R., Duffield, S. J., Braunlich, K., & Conway, B. R. (2025).
+        Color and spatial frequency provide functional signatures of retinotopic visual areas.
+        Journal of Neuroscience, 45(2)."
+
+        :param n_components: number of features to find
+        :param device: hardware to use
+        """
+        self.n_components = n_components
+        self.components = None
+        self.device = device
+        self.metric = dist_metric
+        self.sparsity = sparsity
+        self.intra_weight = intra_class_weight
+        self.inter_weight = inter_class_weight
+
+    def to(self, device):
+        if self.components is not None:
+            self.components = self.components.to(device)
+        self.device = device
+        return self
+
+    def feature_ln(self, comp, degree):
+        return torch.sum(torch.pow(torch.abs(comp), degree), dim=0).mean()
+
+
+    def fit(self, X, y, max_iter=10000, verbose=False, converge_var=.01, degree=2., dist_mask=None, bootstrap_iter=100):
+        """
+        :param converge_var:
+        :param verbose:
+        :param X: items x features
+        :param y: target class
+        :param max_iter:
+        :param dist_mask: can order model to not consider separation between certain targets.
+        :return:
+        """
+        n_features = X.shape[1]
+        X = X.to(self.device)
+        y = y.to(self.device)
+        unique_targets = torch.unique(y)
+        if dist_mask is not None:
+            triu = torch.triu_indices(len(unique_targets), len(unique_targets), offset=1)
+            dist_mask = dist_mask[triu[0], triu[1]]
+        self.components = torch.empty((n_features, self.n_components))
+        self.components = torch.nn.Parameter(torch.nn.init.xavier_normal_(self.components).to(self.device))
+        cur_iter = 0
+        history = []
+        optimizer = torch.optim.AdamW(lr=.01, params=[self.components])
+        converged = False
+        while not converged:
+            if cur_iter >= max_iter:
+                print("WARNING: Failed to converge in", max_iter, "iterations. Could be a solution was found, but the "
+                                                                  "convergence tracker is dumb, just be careful!.")
+                break
+            optimizer.zero_grad()
+            std_components = self.get_norm_components()
+            embed = X @ self.components
+            centers = []
+            loss = torch.zeros((1,)).to(self.device)
+            for t in unique_targets:
+                class_emb = embed[y == t]
+                center = torch.mean(class_emb, dim=0)
+                centers.append(center)
+                var = torch.abs(torch.cov(class_emb.T)).mean()
+                loss += var
+            loss = (loss / len(unique_targets)) * self.intra_weight
+            centers = torch.stack(centers, dim=0)
+            space = torch.pow(torch.pdist(centers), degree)
+            if dist_mask is not None:
+                space = space * dist_mask
+            space = torch.pow(space.mean(), (1 / degree))
+            loss = loss - (self.inter_weight * space)
+            loss = loss + self.sparsity * self.feature_ln(std_components, 1.00)
+            if torch.isnan(loss):
+                raise ValueError("SL: numerical instability, loss is non-finite. Try again. "
+                                 "If recurring, adjust hyperparams")
+            history.append(loss.detach().cpu().item())
+            if verbose:
+                print("iteration", cur_iter, "loss is", history[-1])
+            loss.backward()
+            optimizer.step()
+            optimizer, converged = util.is_converged(history, optimizer, 1, max_lr=.1, t=cur_iter)
+            cur_iter += 1
+        print("done in", cur_iter, "iterations")
+
+    def get_norm_components(self):
+        if self.components is None:
+            print("Must fit first.")
+            return
+        return self.components / torch.linalg.norm(self.components, dim=0)
+
+    def predict(self, X):
+        embed = X.cpu() @ self.components
+        return embed.detach().cpu()
---
+++ neurotools/geometry.py
@@ -0,0 +1,159 @@
+import itertools
+
+import torch
+import numpy as np
+from neurotools import util, stats
+
+_distance_metrics_ = ['euclidian', 'pearson', 'spearman', 'dot', 'cosine' ]
+
+
+def _euclidian_pdist(arr: torch.Tensor, order=2):
+    """
+    arr should be 3D <batch x observations(v) x conditions (k) >
+    if normalize is true, equivalent to pairwise cosine similarity
+    :param arr:
+    :return:
+    """
+    arr = arr.unsqueeze(2)
+    k = arr.shape[1]
+    pd = arr - arr.transpose(1, 2)
+    pd = pd.pow(order)
+    pd = pd.sum(dim=-1)
+    pd = pd.pow(1 / order)
+    indices = torch.triu_indices(k, k, offset=1)
+    return  pd[:, indices[0], indices[1]]
+
+
+def _dot_pairwise(arr: torch.Tensor, normalize=False):
+    """
+    arr should be 3D <batch x observations(v) x conditions (k) >
+    if normalize is true, equivalent to pairwise cosine similarity
+    :param arr:
+    :return:
+    """
+    if len(arr.shape) == 1:
+        arr = arr.reshape(1, arr.shape[0])
+    k = arr.shape[1]
+    if normalize:
+        arr = arr / (arr.norm(dim=2, keepdim=True) + 1e-8)
+    outer = torch.matmul(arr, arr.transpose(-1, -2))  # k x k
+    indices = torch.triu_indices(k, k, offset=1)
+    return outer[:, indices[0], indices[1]]
+
+
+def _pearson_pairwise(arr: torch.Tensor):
+    arr = arr.squeeze()
+    k = arr.shape[1]
+    coef = torch.corrcoef(arr)
+    indices = torch.triu_indices(k, k, offset=1)
+    coef = coef[:, indices[0], indices[1]]
+    return coef.unsqueeze(0)
+
+def pdist_general(X: torch.Tensor, metric, **kwargs):
+    """
+    Slow pdist only for small(ish) number of comparisons with arbitrary distane function.
+    arr should be 3D <batch x observations(v) x conditions (k) >
+    """
+    b, n, f = X.shape
+    device = X.device
+    out_size = (n * (n - 1)) // 2
+    dm = torch.zeros(out_size, device=device).float()
+    k = 0
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            dm[k] = metric(X[:, i, :], X[:, j, :], **kwargs)
+            k += 1
+    return dm
+
+def dissimilarity(beta: torch.Tensor, metric='dot'):
+    if len(beta.shape) == 2:
+        beta = beta.unsqueeze(0)
+    elif len(beta.shape) != 3:
+        raise IndexError("beta should be 3 dimensional and have batch on dim 0, observations on dim 1 and conditions on dim 2")
+    if metric not in _distance_metrics_:
+        raise ValueError('metric must be one of ' + str(_distance_metrics_))
+    elif metric == 'dot':
+        rdm = _dot_pairwise(beta, normalize=False)
+        rdm = max(rdm) - rdm # this may not be ideal
+    elif metric == 'cosine':
+        rdm = 1 - _dot_pairwise(beta, normalize=True) # take cosine distance to be 1 - cosine similarity
+    elif metric == 'pearson':
+        rdm = 1 - _pearson_pairwise(beta)
+    elif metric == 'euclidian':
+        rdm = _euclidian_pdist(beta)
+    else:
+        raise NotImplementedError
+    if torch.sum(rdm < 0) > 0:
+        raise  ValueError("Shouldn't be any negative values in RDM")
+    return rdm
+
+
+def dissimilarity_from_supervised(data, targets, example_weights=None, metric="dot"):
+    """
+    data is batch, examples, features
+    where here batch refers to separate items we're computing the dissimilarity for, and examples refers to
+    examples that have a specific target.
+    """
+    group_labels = list(np.unique(targets))
+    group_means = []
+    for t in group_labels:
+        if example_weights is None:
+            example_weights = torch.ones(len(data)) / len(data)
+        else:
+            assert torch.isclose(example_weights.sum(), torch.tensor([1.]))
+        g = data[:, targets == t, :]
+        group_mean = torch.sum(g*example_weights[:, None, None], dim=1)  # mean across examples
+        group_means.append(group_mean)
+    group_means = torch.stack(group_means, dim=1)  # build "condition" (target) dimension
+    rdm = dissimilarity(group_means, metric)
+    return rdm
+
+
+def pairwise_rsa(data_region_list, rdm_metric='cosine', pairwise_metric='spearman'):
+    """
+    Compute rdms for entries in data_region_list, and their pairwise correlations.
+    :param data_region_list: a list of 2D tensors, each (data_points x features) (or 3d tensor if all regions are same size)
+    :param rdm_metric: metric to use when computing rdms
+    :param pairwise_metric: metric to use when computing pairwise correlations
+    :return:  adjacency matrix, list of rdms for each region
+    """
+    roi_dissimilarity = [dissimilarity(data, metric=rdm_metric) for data in data_region_list]
+    dissim = torch.cat(roi_dissimilarity, dim=0)
+    if pairwise_metric == 'spearman':
+        fxn = stats.spearman_correlation
+    elif pairwise_metric == 'pearson':
+        fxn = stats.pearson_correlation
+    else:
+        raise ValueError
+    corr = pdist_general(dissim, fxn)
+    return corr, dissim
+
+
+def circle_corr(sample, n, metric="rho"):
+    """
+    determine how well the ordering of samples matches the ordering of a circle with n items.
+    Sample is assumed to be the upper triangle of a pairwise distance matrix with size cr(n, 2)
+    Args:
+        n: number of items in circle
+        sample: torch.Tensor <batch, cr(n, 2)>  upper triangle distance matrix.
+    Returns: <batch,> spearman rho or kendalls tau on each batch
+    """
+    # create circle of items and get distance matrix
+    inds = 2 * torch.pi * torch.arange(n) / n
+    circ_coords = torch.stack([torch.cos(inds), torch.sin(inds)], dim=1).unsqueeze(0)
+    circ_dist = _euclidian_pdist(circ_coords).reshape((1, -1))
+    gt_ranks = util.get_ranks(circ_dist, ties=True)
+    sample_ranks = util.get_ranks(sample, ties=True)
+    if metric == "tau":
+        gt_idx = torch.argsort(gt_ranks, dim=1)
+        gt_ranks = util.take_along_axis(gt_ranks, gt_idx, dim=1)
+        sample_ranks = util.take_along_axis(sample_ranks, gt_idx, dim=1)
+        m = stats._ktau_from_ranks(gt_ranks, sample_ranks.unsqueeze(0))
+    elif metric == "rho":
+        m = stats.pearson_correlation(gt_ranks.float(), sample_ranks.float(), dim=1)
+    else:
+        raise ValueError
+    return m
+
+
+
